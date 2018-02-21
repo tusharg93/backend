29391f40f835e695b7e71ba6251a4ce718258b27
tushar93 <tushar@loktra.com>
2018-02-22 00:07:36 +0530

    fix

--- odyssey/v1/form/controllers/form_api.py
+++ odyssey/v1/form/controllers/form_api.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource
 from odyssey.v1.status_codes import *
 from odyssey.v1.form.functions import register_form_data
-from odyssey.v1.vendors.functions import register_vendor_data
 from odyssey.v1.status_codes import OK,INTERNAL_SERVER_ERROR
 from flask import request
 from odyssey import app
--- odyssey/v1/vendors/__init__.py
+++ odyssey/v1/vendors/__init__.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 from odyssey.v1.vendors.controllers.vendor_profile_api import VendorProfileAPI
-from odyssey.v1.vendors.controllers.register_api import RegisterAPI
+from odyssey.v1.vendors.controllers.register_api import VendorRegisterAPI
 
 vendors_blueprint_v1 = Blueprint('vendors_blueprint_v1', __name__)
 api = Api(vendors_blueprint_v1, prefix='/api/v1/vendors')
 
 api.add_resource(VendorProfileAPI, '/profile', strict_slashes=False)
-api.add_resource(RegisterAPI,'/register',strict_slashes=False)
+api.add_resource(VendorRegisterAPI,'/register',strict_slashes=False)
